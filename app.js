64c7d7fd027c8a1014d5793182ea226f7a4bdf54
Charles <charles-emmanuel.dias@etu.upmc.fr>
2013-06-01 17:54:07 +0200

    ok-seems fair

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var args = {
 		SCid : "3f45abb44a7e483b35d0b04047849967",
-		link : ["http://soundcloud.com/whyel/lana-del-rey-summertime"];
+		links : ["https://soundcloud.com/bastilleuk/thinkin-bout-you-feat-o-n-e","https://soundcloud.com/phoenix/trying-to-be-cool-breakbot-remix"],
+		autoplay : true,
+		continuous : true,
 	};
 	var flux = new Flux(args);
--- src/flux.js
+++ src/flux.js
@@ -2,38 +2,112 @@ function Flux(args){
 
 	this.client_id = args.SCid;
 	SC.initialize({client_id:this.client_id});
+
+	this.streams = [];
 	this.tracksLoaded = 0;
-	this.totalTracks = args.links.length;
-	this.tracks = this._linksToTracklist(args.links); // /!\ asynchronous
 	this.currentTrack = 0;
+
+	this.totalTracks = args.links.length;
+	this.autoplay = args.autoplay;
+	this.continuous = args.continuous;
+	this.tracks = this._linksToTracklist(args.links); // /!\ asynchronous
 
 }
 
-
+/*resolves a soundcloud track url's array*/
 Flux.prototype._linksToTracklist = function(list){
 	var tracks = [];
 	var self = this;
 
 	for(var i=0;i<list.length;i++){
-		SC.get("http://api.soundcloud.com/resolve",{url:list[i]}, function(resp){
-			self.tracksLoaded++;
-			if(resp.errors === undefined && resp.kind === "track")
-				tracks.push(resp);
+		SC.get("http://api.soundcloud.com/resolve",{url:list[i]}, function(track){
+			if(track.errors === undefined && track.kind === "track"){
+				tracks.push(track);
+				self._trackToStream(track);
+			}
 			else
+			{
 				console.log("Wrong Soundcloud link");
+				self.totalTracks--;
+			}
 		});
 	}
 	return tracks;
 }
 
-Flux.prototype.togglePlay = function(){
+Flux.prototype._trackToStream = function(track) {
+	var self = this;
+	var params = {}
 
+	if(this.continuous)
+		params.onfinish = function(){self.nextSong()};
+
+	SC.stream("/tracks/"+track.id, params, function(sound){
+  		self.streams.push(sound);
+  		self.tracksLoaded++;
+
+  		if(self.tracksLoaded === self.totalTracks && self.autoplay){
+  			self.togglePlay();
+  		}
+	});
 };
 
-Flux.prototype.setVolume = function(volume){};
 
-Flux.prototype.nextSong = function(){};
+/**			Controls			**/
 
-Flux.prototype.previousSong = function(){};
+Flux.prototype.stop = function() {
+	this.streams[this.currentTrack].stop();
+	this.streams[this.currentTrack].setPosition(0);
+};
 
-Flux.prototype.goToXSec = function(time){};
+Flux.prototype.togglePlay = function(){
+
+	this.streams[this.currentTrack].togglePause();
+	console.log("play:"+this.currentTrack);
+};
+
+Flux.prototype.setVolume = function(volume){
+
+	if(volume >=0 && volume <= 100)
+		this.streams[this.currentTrack].setVolume(volume);
+	else
+		console.log("Error: volume [0;100] you gave "+volume);
+};
+
+Flux.prototype.nextSong = function(){
+
+	this.stop();
+	this.currentTrack = (this.currentTrack+1)%this.totalTracks;
+
+	if(this.autoplay)
+		this.togglePlay();
+};
+
+Flux.prototype.previousSong = function(){
+
+	this.stop();
+	if(this.currentTrack !== 0)
+		this.currentTrack--;
+	else
+		this.currentTrack = this.totalTracks-1;
+
+	if(this.autoplay)
+		this.togglePlay();
+};
+
+Flux.prototype.goToSong = function(song) {
+
+	this.stop();
+	if(song >= 0 && song <= this.totalTracks-1)
+		this.currentTrack = song
+	else
+		console.log("Error: wrong song number");
+
+	if(this.autoplay)
+		this.togglePlay();
+};
+
+Flux.prototype.goToXSec = function(time){
+
+	this.streams[this.currentTrack].setPosition(time*1000);
+};
